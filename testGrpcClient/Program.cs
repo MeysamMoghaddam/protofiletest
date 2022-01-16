800fefccc49b305ebaa328d944b3a47117618000
Meysam Moghaddam <meysam.mogaddam@gmail.com>
2022-01-16 18:56:06 +0330

    First

---
+++ testGrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using testGrpcService;
+
+using var grpcChannel = GrpcChannel.ForAddress("https://localhost:7008/");
+
+var client = new Greeter.GreeterClient(grpcChannel);
+HelloRequest request = new HelloRequest
+{
+    Name = "Meysam",
+    Test = new Test
+    {
+        TestName = "Moghaddam"
+    }
+};
+
+var reply = client.SayHello(request);
+
+Console.WriteLine(reply.Message);
+Console.ReadKey();
